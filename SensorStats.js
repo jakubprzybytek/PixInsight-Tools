a3c77e7a32758c36e8f817b0de8e67fccfbd3d9a
Jakub Przybytek <cubanan@gmail.com>
2018-12-28 13:28:24 +0100

    Update

--- SensorStats.js
+++ SensorStats.js
@@ -9,14 +9,15 @@
 
 function ImageOptions() {
 
-   this.flats = {};
-
    this.biases = {};
+
+   this.darks = {};
+
+   this.flats = {};
 
    this.loadImage = function (imageId) {
       var window = ImageWindow.windowById(imageId);
       if (!window.isNull) {
-         var keywords = window.keywords;
          var fitsHeader = this.readFitsHeader(window.keywords);
          console.writeln("Loaded file: type: " + fitsHeader.imageType + ", Gain: " + fitsHeader.gain + ", exposure: " + fitsHeader.exposure + ", temp: " + fitsHeader.sensorTemp);
 
@@ -26,52 +27,78 @@ function ImageOptions() {
       }
    }
 
+   this.loadImageFromFile = function (directory, fileName) {
+      //console.writeln("Loading: " + fileName);
+      var window = ImageWindow.open(directory + "\\" + fileName);
+      if (!window.isNull) {
+         var fitsHeader = this.readFitsHeader(window[0].keywords);
+         console.writeln("<b>Loaded file</b>: type: " + fitsHeader.imageType + ", Gain: " + fitsHeader.gain + ", exposure: " + fitsHeader.exposure + ", temp: " + fitsHeader.sensorTemp);
+
+         this.registerImage(fitsHeader);
+      } else {
+         console.writeln("Image not loaded");
+      }
+   }
+
+   this.loadImagesFromDirectory = function (directoryRoot) {
+      console.writeln("----------------------------------");
+      console.writeln("<b>Loading images from: " + directoryRoot + "</b>");
+
+      var fileFind = new FileFind;
+      if (fileFind.begin(directoryRoot + "/*.fit")) {
+         do {
+            if (fileFind.name != "." && fileFind.name != "..") {
+               this.loadImageFromFile(directoryRoot, fileFind.name);
+            }
+         } while (fileFind.next());
+      }
+   }
+
    this.registerImage = function (fitsHeader) {
-      if (fitsHeader.imageType == 'FLAT') {
-         if (fitsHeader.gain in this.flats) {
-            this.flats[fitsHeader.gain].push(fitsHeader);
-         } else {
-            this.flats[fitsHeader.gain] = [fitsHeader];
-         }
-      } else if (fitsHeader.imageType == 'BIAS') {
+      if (fitsHeader.imageType == 'BIAS') {
          if (fitsHeader.gain in this.biases) {
             this.biases[fitsHeader.gain].push(fitsHeader);
          } else {
             this.biases[fitsHeader.gain] = [fitsHeader];
          }
+      } else if (fitsHeader.imageType == 'DARK') {
+         if (fitsHeader.gain in this.darks) {
+            this.darks[fitsHeader.gain].push(fitsHeader);
+         } else {
+            this.darks[fitsHeader.gain] = [fitsHeader];
+         }
+      } else if (fitsHeader.imageType == 'FLAT') {
+         if (fitsHeader.gain in this.flats) {
+            this.flats[fitsHeader.gain].push(fitsHeader);
+         } else {
+            this.flats[fitsHeader.gain] = [fitsHeader];
+         }
       }
    }
 
    this.readFitsHeader = function (keywords) {
-     var fitsHeader = {};
-     for (var i = 0; i < keywords.length; i++) {
-      if (keywords[i].name == "IMAGETYP") {
-         fitsHeader.imageType = /'(\w+)\s+'/.exec(keywords[i].value)[1];
-      } else if (keywords[i].name == "EXPOSURE") {
-         fitsHeader.exposure = Number(keywords[i].value);
-         //console.writeln("Found exp ", fitsHeader.exposure);
-      } else if (keywords[i].name == "GAIN") {
-         fitsHeader.gain = Number(keywords[i].value);
-         //console.writeln("Found gain ", fitsHeader.gain);
-      } else if (keywords[i].name == "XPIXSZ") {
-         fitsHeader.xPixelSize = Number(keywords[i].value);
-         //console.writeln("Found x ", fitsHeader.xPixelSize);
-      } else if (keywords[i].name == "YPIXSZ") {
-         fitsHeader.yPixelSize = Number(keywords[i].value);
-         //console.writeln("Found y ", fitsHeader.yPixelSize);
-      } else if (keywords[i].name == "CCD-TEMP") {
-         fitsHeader.sensorTemp = Number(keywords[i].value);
-         //console.writeln("Found temp ", fitsHeader.sensorTemp);
-      } else if (keywords[i].name == "SET-TEMP") {
-         fitsHeader.setTemp = Number(keywords[i].value);
-         //console.writeln("Found set temp ", fitsHeader.setTemp);
-      } else if (keywords[i].name == "DATE-LOC") {
-         fitsHeader.localDate = Date(keywords[i].value);
-         //console.writeln("Found date ", fitsHeader.localDate);
+      var fitsHeader = {};
+      for (var i = 0; i < keywords.length; i++) {
+         if (keywords[i].name == "IMAGETYP") {
+            fitsHeader.imageType = /'(\w+)\s+'/.exec(keywords[i].value)[1];
+         } else if (keywords[i].name == "EXPOSURE") {
+            fitsHeader.exposure = Number(keywords[i].value);
+         } else if (keywords[i].name == "GAIN") {
+            fitsHeader.gain = Number(keywords[i].value);
+         } else if (keywords[i].name == "XPIXSZ") {
+            fitsHeader.xPixelSize = Number(keywords[i].value);
+         } else if (keywords[i].name == "YPIXSZ") {
+            fitsHeader.yPixelSize = Number(keywords[i].value);
+         } else if (keywords[i].name == "CCD-TEMP") {
+            fitsHeader.sensorTemp = Number(keywords[i].value);
+         } else if (keywords[i].name == "SET-TEMP") {
+            fitsHeader.setTemp = Number(keywords[i].value);
+         } else if (keywords[i].name == "DATE-LOC") {
+            fitsHeader.localDate = Date(keywords[i].value);
+         }
       }
-     }
 
-     return fitsHeader;
+      return fitsHeader;
    }
 
    this.reportCollection = function (collection) {
@@ -84,12 +111,16 @@ function ImageOptions() {
 
    this.report = function () {
       console.writeln("----------------------------------");
+      console.writeln("<b>Images loaded</b>:");
 
-      console.write("Flats: ");
+      console.write("<b>Flats</b>: ");
       this.reportCollection(this.flats);
 
-      console.write("Biases: ");
+      console.write("<b>Biases</b>: ");
       this.reportCollection(this.biases);
+
+      console.write("<b>Darks</b>: ");
+      this.reportCollection(this.darks);
    }
 
 }
@@ -98,9 +129,7 @@ function main() {
 
    var imageOptions = new ImageOptions();
 
-   imageOptions.loadImage("Flat_10sec_OIII_1x1_0010_g0_20C");
-   imageOptions.loadImage("Flat_10sec_OIII_1x1_0020_g0_20C");
-   imageOptions.loadImage("Bias_0sec_None_1x1_g0_20C_0010");
+   imageOptions.loadImagesFromDirectory("d:\\Astro\\_Lib\\ASI1600MM-C Pro\\Stats");
 
    imageOptions.report();
 }
